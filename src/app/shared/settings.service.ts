d37b670abc5d6dea85d4e25df086da3f8802e7e1
Mirko Mälicke <mirko@maelicke-online.de>
2018-05-22 11:34:57 +0200

    moved to Angular 6 and yarn

---
+++ src/app/shared/settings.service.ts
@@ -0,0 +1,29 @@
+import {BehaviorSubject} from 'rxjs/internal/BehaviorSubject';
+
+export class SettingsService {
+  private settings: {
+    elasticHost: string[],
+    refreshStatus: number
+  };
+
+  // this should be removed into a subject
+  defaultListStyle: BehaviorSubject<string>;
+  elasticHost: BehaviorSubject<string>;
+  refreshStatus: BehaviorSubject<number>;
+
+  constructor() {
+    this.settings = {
+      'elasticHost': ['http://localhost:9200'],
+      'refreshStatus': 20000
+    };
+
+    // DEV: prepopulation
+    this.defaultListStyle = new BehaviorSubject('table');
+    this.elasticHost = new BehaviorSubject('http://localhost:9200');
+    this.refreshStatus = new BehaviorSubject(20000);
+  }
+
+  getSetting(settingName: string) {
+    return this.settings[settingName];
+  }
+}
